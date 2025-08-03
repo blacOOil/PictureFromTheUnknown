e0531a000402c9038086ba0a8be5cf78d17b9f44
blacOOil <111561598+blacOOil@users.noreply.github.com>
2025-08-03 18:23:19 +0700

    OilPaintedFilter

--- Assets/Script/RedRoom/RedRoomControll.cs
+++ Assets/Script/RedRoom/RedRoomControll.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RedRoomControll : MonoBehaviour
 {
+    public bool IsredroomActived = false;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
